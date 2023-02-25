4524e9e091bd8064da8665474d2a2f24b5f9b8eb
Marina18241 <9mariam7@gmail.com>
2023-02-25 23:03:59 +0300

    Rename edit -> modify

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 
 
 
-def test_edit_contact(app):
+def test_modify_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(middlename="edit middlename", firstname="edit firstname"))
     app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 
 
 
-def test_edit_group(app):
+def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="edit name", header="edit header", footer="edit footer"))
     app.session.logout()
