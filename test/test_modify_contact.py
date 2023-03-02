254b576ce15f4776ed38871ba5dc3656ae80b950
Marina18241 <9mariam7@gmail.com>
2023-03-02 19:30:01 +0300

    Вынесение kogin и logout в conftest

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,12 +4,9 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
                 title="title test", company="company test", address="address test", home="+7843564224",
                 mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru", email2="test2@yandex.ru",
                 email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August", byear="1998", aday="13",
                 amonth="August", ayear="1999", address2="secondary address test", phone2="secondary home test",
                 notes="test notes test"))
-
-    app.session.logout()
