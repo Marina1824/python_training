ec61493adee4ab6e090db4b9a2d165be004c4510
Marina18241 <9mariam7@gmail.com>
2023-03-03 23:04:35 +0300

    удаление и модификация групп и контактов
    задание 9

--- fixture/contact.py
+++ fixture/contact.py
@@ -8,105 +8,78 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
 
-    def open_new_contact(self):
+    def change_select_value(self,field_name,text):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if text is not None:
+            Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
 
-    def create(self, contact):
+    def change_field_value(self,field_name,text):
         wd = self.app.wd
-        self.open_new_contact()
-        # fill group creation
-        self.fill_contact_form(contact)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+        self.change_field_value("firstname",contact.firstname)
+        self.change_field_value("middlename",contact.middlename)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("title",contact.title)
+        self.change_field_value("company",contact.company)
+        self.change_field_value("address",contact.address)
+        self.change_field_value("home",contact.home)
+        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("work", contact.work)
+        self.change_field_value("fax", contact.fax)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3",contact.email3)
+        self.change_field_value("homepage",contact.homepage)
+        self.change_select_value("bday",contact.bday)
+        self.change_select_value("bmonth",contact.bmonth)
+        self.change_field_value("byear",contact.byear)
+        self.change_select_value("aday", contact.aday)
+        self.change_select_value("amonth", contact.amonth)
+        self.change_field_value("ayear",contact.ayear)
+        self.change_field_value("address2",contact.address2)
+        self.change_field_value("phone2",contact.phone2)
+        self.change_field_value("notes",contact.notes)
 
-
-    def edit_first_contact(self, contact):
+    def modify_first_contact(self, contact):
         wd = self.app.wd
         self.open_home()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        #edit
+        self.select_first_contact()
+        #open modification form
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         self.fill_contact_form(contact)
         # submit group creation
         wd.find_element_by_name("update").click()
+        self.open_home()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_home()
+        wd.find_element_by_link_text("add new").click()
+        # fill group creation
+        self.fill_contact_form(contact)
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        self.open_home()
+
     def delete_first_contact(self):
         wd = self.app.wd
         self.open_home()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
+
+    def select_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_home()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,9 +4,25 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.contact.edit_first_contact(Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
-                title="title test", company="company test", address="address test", home="+7843564224",
-                mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru", email2="test2@yandex.ru",
-                email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August", byear="1998", aday="13",
-                amonth="August", ayear="1999", address2="secondary address test", phone2="secondary home test",
-                notes="test notes test"))
+    if app.contact.count() == 0:
+        app.contact.create(
+            Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
+                    title="title test", company="company test", address="address test", home="+7843564224",
+                    mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru",
+                    email2="test2@yandex.ru",
+                    email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August", byear="1998", aday="13",
+                    amonth="August", ayear="1999", address2="secondary address test", phone2="secondary home test",
+                    notes="test notes test"))
+    app.contact.modify_first_contact(Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
+                    title="title test", company="company test", address="address test", home="+7843564224",
+                    mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru",
+                    email2="test2@yandex.ru",
+                    email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August", byear="1998", aday="13",
+                    amonth="August", ayear="1999", address2="secondary address test", phone2="secondary home test",
+                    notes="test notes test"))
+
+def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="edit"))
+    app.contact.modify_first_contact(Contact(firstname="edit"))
+
