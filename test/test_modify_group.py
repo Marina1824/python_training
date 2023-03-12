d64bd0f375e25186273544af71848645bb104268
Marina18241 <9mariam7@gmail.com>
2023-03-12 18:44:58 +0300

    Проверка равенства объектов и списков
    4 модуль, 2 лекции.
    Загружаем информацию из тестируемого приложения в виде списков
    Проверка равенства объектов и списков

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,13 +2,18 @@
 from model.group import Group
 
 
-
 def test_modify_group_name(app):
-    if app.group.count()==0:
+    old_groups = app.group.get_group_list()
+    if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="edit name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
-    if app.group.count()==0:
+    old_groups = app.group.get_group_list()
+    if app.group.count() == 0:
         app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(header="edit header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
