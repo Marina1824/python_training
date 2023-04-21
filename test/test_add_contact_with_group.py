365887492b0fc40b99298619e36d6e2b05d0b281
Marina18241 <9mariam7@gmail.com>
2023-04-21 22:44:29 +0300

    Добавление контакта в группу
    Добавление проверок. Берется контакт который не добавлен в группу

--- test/test_add_contact_with_group.py
+++ test/test_add_contact_with_group.py
@@ -5,21 +5,26 @@ from model.contact import Contact
 from model.group import Group
 
 def test_add_contact_with_group(app, db, orm ):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
-                    title="title test", company="company test", address="address test", home="+7843564224",
-                   mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru",
-                    email2="test2@yandex.ru",
-                    email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August", byear="1998", aday="13",
-                   amonth="August", ayear="1999", address2="secondary address test", phone2="secondary home test",
-                    notes="test notes test"))
-    contacts = app.contact.get_contact_list()
     group_list = app.contact.get_group_list()
     if len(group_list) == 0:
         app.group.create(Group(name="test", header="header", footer="test footer"))
         app.contact.open_home()
         group_list = app.contact.get_group_list()
-    random_contact = random.choice(contacts)
+    random_group_id = random.choice(group_list)
+    contacts_not_in_group = orm.get_contacts_not_in_group_by_id(random_group_id)
+    if len(contacts_not_in_group) == 0 or app.contact.count() == 0:
+        app.contact.create(Contact(firstname="edit", middlename="middlename", lastname="last test", nickname="nicktest",
+                                   title="title test", company="company test", address="address test",
+                                   home="+7843564224",
+                                   mobile="+7946545455", work="+9373632923", fax="+432423", email="test@mail.ru",
+                                   email2="test2@yandex.ru",
+                                   email3="test3@yandex.ru", homepage="homepage", bday="11", bmonth="August",
+                                   byear="1998", aday="13",
+                                   amonth="August", ayear="1999", address2="secondary address test",
+                                   phone2="secondary home test",
+                                   notes="test notes test"))
+        contacts_not_in_group = orm.get_contacts_not_in_group_by_id(random_group_id)
+    random_contact = random.choice(contacts_not_in_group)
     app.contact.select_contact_by_id(random_contact.id)
     random_group_id = random.choice(group_list)
     app.contact.change_select_value_by_value("to_group", random_group_id)
